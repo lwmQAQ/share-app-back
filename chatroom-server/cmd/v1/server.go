019d22264d1accd4c813b252120a3be7e9980ffe
MADAO <3191711818@qq.com>
2024-12-02 20:03:08 +0800

    tcp服务启动

--- chatroom-server/cmd/v1/server.go
+++ chatroom-server/cmd/v1/server.go
@@ -1,5 +1,31 @@
 package v1
 
+import (
+	"chatroom-server/jinx/jnet"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func ServerStart() {
+	server := jnet.NewServer()
+	// 使用 goroutine 启动服务器
+	go func() {
+		server.Start()
+	}()
+
+	// 捕获 Ctrl+C 或终止信号
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	// 阻塞等待信号
+	<-stopChan
+	fmt.Println("\n收到终止信号，正在关闭服务器...")
+
+	// 优雅关闭服务器
+	server.Stop()
+
+	fmt.Println("Server stopped.")
 
 }
--- chatroom-server/main.go
+++ chatroom-server/main.go
@@ -1,5 +1,7 @@
 package chatroomserver
 
-func main() {
+import v1 "chatroom-server/cmd/v1"
 
+func main() {
+	v1.ServerStart()
 }
